21e1a8a43de01cc2016a92bcd21a3b3de54e3006
Matthew Loar <matthew@loar.name>
2009-02-02 13:25:03 +0000

    Add a shoddy script to handle the pipe.
    --HG--
    branch : afsbak

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+VOS=/usr/bin/vos
+TIME="0"
+VOSARGS=-localauth
+TARVOL=/usr/local/sbin/tarvol
+
+# Need the useless short option to make getopt not go into stupid mode
+ARGS=`getopt -o "q" -s sh -l "newer:" -- "$@"`
+if [[ $? -ne 0 ]]; then
+    echo "getopt failed"
+    exit 1
+fi
+
+eval set -- "$ARGS"
+
+while [[ "$1" != "--" ]]; do
+    if [[ "$1" = "--newer" ]]; then
+        TIME=`date "--date=$2" "+%m/%d/%Y %H:%M"`
+        shift
+    fi
+    shift
+done
+shift
+
+$VOS dump $1 -time "$TIME" $VOSARGS | $TARVOL -cv
