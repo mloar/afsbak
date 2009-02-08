74c3b1fb9c470c3b4e4393984dec4454401481e6
Matthew Loar <matthew@loar.name>
2009-02-08 04:53:20 +0000

    Fix segfault because I was doing something stupid with the STL. I should know better.
    --HG--
    branch : afsbak

--- storage.cpp
+++ storage.cpp
@@ -10,10 +10,17 @@
 std::map<int, std::string> m_entries;
 void add(int vnode, const char *file)
 {
-  m_entries.insert(std::make_pair(vnode, file));
+    m_entries.insert(std::make_pair(vnode, file));
 }
 
 const char *get(int vnode)
 {
-  return m_entries.find(vnode)->second.c_str();
+    if (m_entries.find(vnode) != m_entries.end())
+    {
+        return m_entries.find(vnode)->second.c_str();
+    }
+    else
+    {
+        return NULL;
+    }
 }
